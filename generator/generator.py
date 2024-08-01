184cac00014f6294c76970359a90c292f8d5001a
savchukura <savcukura@gmail.com>
2024-08-01 18:32:10 +0300

    Was added Data and generator for creating random data

---
+++ generator/generator.py
@@ -0,0 +1,20 @@
+import random
+from data.data import Person
+from faker import Faker
+faker_en = Faker()
+Faker.seed()
+
+
+def generated_person():
+    yield Person(
+        full_name=faker_en.first_name() + " " + faker_en.last_name(),
+        firstname=faker_en.first_name(),
+        lastname=faker_en.last_name(),
+        age=random.randint(20, 80),
+        salary=random.randint(1000, 100000),
+        department=faker_en.job(),
+        email=faker_en.email(),
+        current_address=faker_en.address(),
+        permanent_address=faker_en.address(),
+        mobile=faker_en.msisdn()
+    )
